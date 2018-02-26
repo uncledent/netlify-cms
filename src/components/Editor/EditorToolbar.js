af3520ca4f4989a07146ba1710229b2cff3358d0
Denis Beklarov <denis.beklarov.extern@baimos.com>
2018-02-26 17:08:05 +0100

    Removed download button

--- src/components/Editor/EditorToolbar.js
+++ src/components/Editor/EditorToolbar.js
@@ -80,7 +80,7 @@ export default class EditorToolbar extends React.Component {
 
   renderDownloadButton = () => {
     const { onPersist, slug, collectionName, downloadUrl } = this.props;
-    if (collectionName!=="setting") {
+    if (collectionName!=="settings" && collectionName!=="confirmations") {
         return <a className="nc-collectionPage-topNewButton"  href={`${downloadUrl}/${collectionName}/${slug}.html`} download={`${collectionName}_${slug}.html`}>
         Download
       </a>
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import './index.css';
 /**
  * Log the version number.
  */
-console.log(`Netlify CMS version ${NETLIFY_CMS_VERSION} von 26.02.18 build 2`);
+console.log(`Netlify CMS version ${NETLIFY_CMS_VERSION} von 26.02.18 build 3`);
 
 /**
  * Create mount element dynamically.
